e9a2b14ecb2e42439a6da3a30847d583d166ed6f
Sujit Sharma <shuzeet15@gmail.com>
2024-03-31 08:15:30 +0545

    completed

---
+++ Product/product.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import { productCategories } from "../Constants/general.constant.js";
+
+//set rule
+const productSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        trim:true,
+        maxlength:30,
+        required:true
+    },
+    brand:{
+        type:String,
+        trim:true,
+        maxlength:30,
+        required:true
+    },
+    price:{
+        type:Number,
+        min:0,
+        required:true,
+    },
+    quantity:{
+        type:Number,
+        min:0,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+        enum:productCategories
+    },
+    image:{
+        type:String,
+        required: false,
+        default: null,
+        nullable: true,
+    },
+    description:{
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 1000,
+    },
+    ownerId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "users",
+    },
+},
+{
+    timestamps:true
+}
+)
+
+//create table
+const Product= mongoose.model("Product",productSchema);
+
+export default Product;
